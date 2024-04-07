d0fe0561b84db76db89966b4412784b920280273
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-04-07 20:20:57 +0200

    add component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-export const App = () => {
-  return <div>Find me in App.jsx!</div>;
+import React from "react";
+import TodoList from "./TodoList";
+import TodoForm from "./TodoForm";
+import { TodoProvider } from "./TodoContext";
+
+const App = () => {
+  return (
+    <TodoProvider>
+      <div>
+        <h1>Todo App</h1>
+        <TodoForm />
+        <TodoList />
+      </div>
+    </TodoProvider>
+  );
 };
+
+export default App;
