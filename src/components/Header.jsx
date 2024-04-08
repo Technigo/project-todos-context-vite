01afffa6df2b9d4387c459eb33c5743500cec0a7
Martin Jönsson <martin.t.joensson@gmail.com>
2024-04-08 14:51:25 +0200

    Added task counter

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
 import "./Header.css";
 import { useAppData } from "../themes/AppTheme";
+import { useTask } from "../themes/TaskTheme";
 
 export const Header = () => {
-  const { name, appContent } = useAppData();
+    const { name, appContent } = useAppData();
+    const { taskData, addTask } = useTask();
+
   console.log(name, appContent);
 
   return (
     <header>
-      <h1>{appContent.heading}</h1>
+          <h1>{appContent.heading}</h1>
+          <p>Task count: {taskData.length}</p>
     </header>
   );
 };
