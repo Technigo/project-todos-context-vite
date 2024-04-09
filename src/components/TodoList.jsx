0065f836aa3b970c53b0fb1a92bdc380e6b13741
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-09 23:32:03 +0200

    Separate uncompleted and completed todos by using Todo component

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,36 +1,47 @@
 // A component to display a list of todo items
 import { useContext } from "react";
 import { TodoContext } from "./TodoContext";
+import { Todo } from "./Todo";
 import moment from "moment";
 
 export const TodoList = () => {
   // Extract todos and the toggleTodo function from our TodoContext.
   const { todos, toggleTodo } = useContext(TodoContext);
 
-  // Count for completed and uncompleted tasks
   // .filter: keeps all the items that matches (returns "true" in ((todo) => todo.completed))
-  const completedCount = todos.filter((todo) => todo.completed).length;
-  const uncompletedCount = todos.length - completedCount;
+  const completedTodos = todos.filter((todo) => todo.completed);
+  const uncompletedTodos = todos.filter((todo) => !todo.completed);
+
+  // Count for completed and uncompleted tasks
+  const completedCount = completedTodos.length;
+  const uncompletedCount = uncompletedTodos.length;
 
   return (
     <div className="list-container">
       <div>Completed: {completedCount}</div>
       <div>Uncompleted: {uncompletedCount}</div>
       <ul>
-        {todos.map((todo) => (
-          <li
-            key={todo.id} // Use the unique id (= Date.now() thing) of each todo for the key prop.
-            style={{ textDecoration: todo.completed ? "line-through" : "none" }} // If the todo is completed, strike through the text; if not, display normally.
-          >
-            <input
-              className="checkbox"
-              type="checkbox"
-              checked={todo.completed} // The checkbox is checked if the todo is marked as completed.
-              onChange={() => toggleTodo(todo.id)} // When you click the checkbox, the toggleTodo function is called with the todo's ID to switch its "completed" status between done and not done.
-            />
-            {todo.todo}{" "}
-            <span>{moment(todo.createdAt).format("YYYY-MM-DD HH:mm")}</span>{" "}
-          </li>
+        <h2>Uncompleted</h2>
+        {uncompletedTodos.map((todo) => (
+          // create Todo.jsx component to handle both completed and uncompleted todos
+          // these props (id, completed, todo...) are the data that Todo.jsx needs
+          <Todo
+            id={todo.id}
+            completed={todo.completed}
+            todo={todo.todo}
+            createdAt={todo.createdAt}
+            onChange={() => toggleTodo(todo.id)}
+          />
+        ))}
+        <h2>Completed</h2>
+        {completedTodos.map((todo) => (
+          <Todo
+            id={todo.id}
+            completed={todo.completed}
+            todo={todo.todo}
+            createdAt={todo.createdAt}
+            onChange={() => toggleTodo(todo.id)}
+          />
         ))}
       </ul>
     </div>
