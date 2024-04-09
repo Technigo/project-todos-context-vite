6da7ef0eeeceb17e6dc28fd3537e0854000ca6a0
Mai Kanetaka <maikanetaka@gmail.com>
2024-04-09 22:44:30 +0200

    Add explaination for TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,29 +1,31 @@
+// A component to display a list of todo items
 import { useContext } from "react";
 import { TodoContext } from "./TodoContext";
 import moment from "moment";
 
 export const TodoList = () => {
+  // Extract todos and the toggleTodo function from our TodoContext.
   const { todos, toggleTodo } = useContext(TodoContext);
 
   return (
     <div className="list-container">
-    <ul>
-      {todos.map((todo) => (
-        <li
-          key={todo.id}
-          style={{ textDecoration: todo.completed ? "line-through" : "none" }}
-        >
-          <input
-          className="checkbox"
-            type="checkbox"
-            checked={todo.completed}
-            onChange={() => toggleTodo(todo.id)}
-          />
-          {todo.todo} -{" "}
-          <span>{moment(todo.createdAt).format("YYYY-MM-DD HH:mm")}</span>{" "}
-        </li>
-      ))}
-    </ul>
+      <ul>
+        {todos.map((todo) => (
+          <li
+            key={todo.id} // Use the unique id (= Date.now() thing) of each todo for the key prop.
+            style={{ textDecoration: todo.completed ? "line-through" : "none" }} // If the todo is completed, strike through the text; if not, display normally.
+          >
+            <input
+              className="checkbox"
+              type="checkbox"
+              checked={todo.completed} // The checkbox is checked if the todo is marked as completed.
+              onChange={() => toggleTodo(todo.id)} // When you click the checkbox, the toggleTodo function is called with the todo's ID to switch its "completed" status between done and not done.
+            />
+            {todo.todo} {" "}
+            <span>{moment(todo.createdAt).format("YYYY-MM-DD HH:mm")}</span>{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
