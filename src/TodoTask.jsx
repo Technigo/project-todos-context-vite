ca07b30fbc3760231360faf0432c9214af73d627
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-04-09 20:56:36 +0200

    cross out task done

--- src/TodoTask.jsx
+++ src/TodoTask.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { TodoContext } from "./TodoContext";
+import clsx from "clsx";
 import "./TodoTask.css";
 
 export const TodoTask = ({ todo, index }) => {
   const todoContext = useContext(TodoContext);
 
   return (
-    <div className="TodoTask">
+    <div className={clsx("TodoTask", { "TodoTask-done": todo.done })}>
       <label className="TodoTask-label">
         <input
           type="checkbox"
