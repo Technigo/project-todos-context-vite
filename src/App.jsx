499970d5a92554bf20d282a369df51cc881f5dc3
fridaforser <fridaforser@gmail.com>
2024-04-09 17:47:32 +0200

    Added components in App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-export const App = () => {
-  return <div>Find me in App.jsx!</div>;
-};
+import "./index.css"
+import React from 'react';
+import { TodoList } from './components/TodoList.jsx'; 
+import { TodoForm } from './components/TodoForm.jsx';
+import { TodoProvider } from './contexts/TodoContext.jsx';
+import { CheckboxProvider } from './contexts/CheckboxContext.jsx';
+
+  export const App = () => {
+    return (
+      <TodoProvider>
+      <CheckboxProvider>
+        <div>
+          <h1>Todo App</h1>
+          <TodoForm />
+          <TodoList />
+        </div>
+        </CheckboxProvider>
+      </TodoProvider>
+    );
+  };
+
+  export default App;
