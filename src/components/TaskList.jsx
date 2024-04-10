adabae2f5aaa2a3f1f9feb41bcf90f98b870aae0
izzibizz <contact@izabellind.com>
2024-04-10 15:00:41 +0200

    fixed id to be unique for each task by using the moment.js time and fixed the empty imput validation

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -13,41 +13,49 @@ const TaskList = ({ data }) => {
     <>
       {(data === 'ToDo' ? ToDo : complete).map((item) => (
         //replace the value of the key and some attr with item.timestamp later
-        <li key={item.task}>
-            <div className="task-item">
-              <div className="checkbox">
+        <li key={item.createdAt}>
+          <div className="task-item">
+            <div className="checkbox">
               <input
-                  type="checkbox"
-                  id={item.task}
-                  // value={item.task}
-                  defaultChecked={complete.includes(item)}
-                  onChange={() => {
-                    if (data === 'ToDo') {
-                      addComplete(item)
-                      removeToDo(item)
-                    } else {
-                      addToDo(item.task)
-                      removeComplete(item)
-                    }
-                  }}
-                />
-                <img src={unChecked} alt="unchecked checkbox" className="unchecked checkbox" />
-                <img src={checked} alt="checked checkbox" className="checked checkbox" />
-              </div>
-              <div className="task-content">
-              <label htmlFor={item.task}>{item.task}</label>
-            <img
-              src={trash}
-              className="trashbin"
-              onClick={() => {
-                if (data === 'ToDo') {
-                  removeToDo(item)
-                } else {
-                  removeComplete(item)
-                }
-              }}
-            />
-          </div>
+                type="checkbox"
+                id={item.createdAt}
+                // value={item.task}
+                defaultChecked={complete.includes(item)}
+                onChange={() => {
+                  if (data === 'ToDo') {
+                    addComplete(item)
+                    removeToDo(item)
+                  } else {
+                    addToDo(item.task)
+                    removeComplete(item)
+                  }
+                }}
+              />
+              <img
+                src={unChecked}
+                alt="unchecked checkbox"
+                className="unchecked checkbox"
+              />
+              <img
+                src={checked}
+                alt="checked checkbox"
+                className="checked checkbox"
+              />
+            </div>
+            <div className="task-content">
+              <label htmlFor={item.createdAt}>{item.task}</label>
+              <img
+                src={trash}
+                className="trashbin"
+                onClick={() => {
+                  if (data === 'ToDo') {
+                    removeToDo(item)
+                  } else {
+                    removeComplete(item)
+                  }
+                }}
+              />
+            </div>
           </div>
           <p>{item.createdAt.calendar()}</p>
         </li>
--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-import { useToDo } from "../context/ToDoContext";
-import "../styling/ToDoForm.css";
+import { useState } from 'react'
+import { useToDo } from '../context/ToDoContext'
+import '../styling/ToDoForm.css'
 
 const ToDoForm = () => {
-  const [newTodo, setNewTodo] = useState("");
-  const { addToDo } = useToDo();
-  // console.log(ToDo[0].task)
+  const [newTodo, setNewTodo] = useState('')
+  const { addToDo } = useToDo()
 
-  const handleInputChange = e => {
-    setNewTodo(e.target.value);
-  };
+  const handleInputChange = (e) => {
+    setNewTodo(e.target.value)
+  }
 
-  const handleSubmit = e => {
-    e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault()
     //validate the task - avoid empty input
-    console.log(newTodo);
-    addToDo(newTodo);
-    setNewTodo("");
-  };
+    console.log(newTodo)
+    addToDo(newTodo)
+    setNewTodo('')
+  }
   return (
     <div className="section todo-form">
       <h1>What do you need to do today?</h1>
@@ -28,10 +27,16 @@ const ToDoForm = () => {
           placeholder="write your task here..."
           onChange={handleInputChange}
         />
-        <button type="submit" className="button add">Add to list</button>
+        <button
+          disabled={newTodo ? false : true}
+          type="submit"
+          className="button add"
+        >
+          Add to list
+        </button>
       </form>
     </div>
-  );
-};
+  )
+}
 
-export default ToDoForm;
+export default ToDoForm
