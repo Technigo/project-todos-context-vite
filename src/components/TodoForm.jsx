e87d8f5f9524d934efef820506b439e39a40779a
AHPIXI <148851757+AHPIXI@users.noreply.github.com>
2024-04-10 14:06:36 +0200

    added inputfield and button (#1)

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,35 @@
-import "./TodoForm.css"
+import { useState } from "react";
+import "./TodoForm.css";
 
 export const TodoForm = () => {
-    return (
-        <div className="form-container">
-            <h2>Add a todo</h2>
-        </div>
-    )
-}
+  //     const { todo, addTodo } = useTodo()
+  // const [newTodo, setNewTodo] = useState({
+  //     todotext: ""
+  // })
+
+  // const handleChange = event => {
+  //     setNewTodo({...newTodo,[event.target.name]: event.target.value})
+  // }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //   addTodo(newTodo)
+    //   setNewTodo(({ todotext: "" }))
+    console.log(event);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>Create new todo</label>
+        <input
+          type="text"
+          name="todotext"
+          placeholder="Add a new todo here..."
+        />
+        <button>Add task</button>
+      </form>
+    </>
+  );
+};
+
+// value={newTodo.todotext}
+// onChange={handleChange}
