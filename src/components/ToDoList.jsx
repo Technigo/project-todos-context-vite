65c24ab0be057d345c7eb7b47f5b9eb323fd4407
Frida Svensson <fridamarisvensson@gmail.com>
2024-04-10 22:36:30 +0200

    create count for tasks

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -4,12 +4,23 @@ import { useToDoContext } from "../contexts/ToDoContext";
 export const ToDoList = () => {
   const { toDos, toggleComplete, clearToDos } = useToDoContext();
 
+  const amountOfTasks = toDos.length;
+
+  const completedTasks = () => {
+    return toDos.filter((toDo) => toDo.completed);
+  };
+
   const handleClearAll = () => {
     clearToDos();
   };
 
   return (
     <div>
+      <div className="task-amount">
+        <p>
+          Completed tasks: {completedTasks().length}/{amountOfTasks}
+        </p>
+      </div>
       <h2>ToDo List</h2>
       <ul>
         {toDos.map((toDo, index) => (
