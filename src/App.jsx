61793664f1790e567b311151895d8401807fe1a7
Frida Svensson <fridamarisvensson@gmail.com>
2024-04-12 00:14:23 +0200

    add background and set some media queries for it

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,13 @@ import { ToDoForm } from "./components/ToDoForm";
 import { ToDoList } from "./components/ToDoList";
 import { TodaysDate } from "./components/TodaysDate";
 import { Header } from "./components/Header";
+import backGround from "./assets/background-blobs.svg";
+import "./App.css";
 
 const App = () => {
   return (
     <>
+      <img src={backGround} className="background" alt="Background" />
       <Header />
       <ToDoProvider>
         <div>
