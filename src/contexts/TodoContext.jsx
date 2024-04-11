a9ebc698113b07d5e2ad4424ec6bca399aa9f32e
fridaforser <fridaforser@gmail.com>
2024-04-11 17:58:27 +0200

    Fixed context page

--- src/contexts/TodoContext.jsx
+++ src/contexts/TodoContext.jsx
@@ -1,11 +1,44 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
 const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
   const [todo, setTodo] = useState([]);
 
-  /* const [todo, setTodo] = useState(() => {
+  const addTodo = (newTodo) => {
+    setTodo((prevTodos) => [...prevTodos, newTodo]);
+  };
+
+  function removeToDo(id) {
+    const removeItem = todo.filter((task) => {
+      return task.id !== id;
+    });
+    setTodo(removeItem);
+  }
+
+  const clearList = () => {
+    alert(`Are you sure you want to remove all tasks from the list?`);
+    setTodo([]);
+  };
+
+  return (
+    <TodoContext.Provider
+      value={{
+        todo,
+        setTodo,
+        addTodo,
+        clearList,
+        removeToDo,
+      }}
+    >
+      {children}
+    </TodoContext.Provider>
+  );
+};
+
+export const useTodo = () => useContext(TodoContext);
+
+/* const [todo, setTodo] = useState(() => {
     const savedTodos = localStorage.getItem("todo");
     if (savedTodos) {
       return JSON.parse(savedTodos);
@@ -17,28 +50,3 @@ export const TodoProvider = ({ children }) => {
   useEffect(() => {
     localStorage.setItem("todo", JSON.stringify(todo));
   }, [todo]); */
-
-  const addTodo = (newTodo) => {
-    setTodo((prevTodos) => [...prevTodos, newTodo]);
-  };
-
-  function removeToDo(id) {
-    const removeItem = todo.filter((task) => {
-      return task.id !== id;
-    });
-    setTodo(removeItem);
-    console.log(id);
-  }
-
-  const clearList = () => {
-    setTodo([]);
-  };
-
-  return (
-    <TodoContext.Provider value={{ todo, addTodo, removeToDo, clearList }}>
-      {children}
-    </TodoContext.Provider>
-  );
-};
-
-export const useTodo = () => useContext(TodoContext);
