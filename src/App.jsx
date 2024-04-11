dd5c89a2eed77e7b65ded8dc8e6c161cd1fe83a9
fridaforser <fridaforser@gmail.com>
2024-04-11 17:57:39 +0200

    Removed checkbox context page from App

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import React from "react";
 import { TodoList } from "./components/TodoList.jsx";
 import { TodoForm } from "./components/TodoForm.jsx";
 import { TodoProvider } from "./contexts/TodoContext.jsx";
-import { CheckboxProvider } from "./contexts/CheckboxContext.jsx";
 
 export const App = () => {
   return (
     <TodoProvider>
-      <CheckboxProvider>
-        <div>
+        <div className="container">
           <h1>Todo App</h1>
           <TodoForm />
           <TodoList />
         </div>
-      </CheckboxProvider>
     </TodoProvider>
   );
 };
