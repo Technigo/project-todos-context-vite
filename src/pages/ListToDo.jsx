6088601aa3949aba56b0a9e0c0c350778c555618
Citronskal <yiaswift@gmail.com>
2024-04-11 18:47:43 +0200

    use moment js for date, add category tag

--- src/pages/ListToDo.jsx
+++ src/pages/ListToDo.jsx
@@ -1,19 +1,15 @@
-
+import { useToDos } from "../ToDoContext";
 
 export const ListToDo = () => {
 
-
+  const 
 
   return (
     <div>
-        <h1>All</h1>
-        <p>show number of tasks</p>
-        <div>
-pass down created tasks
-        </div>
-        <button>+</button>
-
-
+      <h1>All</h1>
+      <p>show number of tasks</p>
+      <div>pass down created tasks</div>
+      <button>+</button>
     </div>
-  )
-}
+  );
+};
