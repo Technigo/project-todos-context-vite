d22d59e802fe8693d8ed5f01950659c6bf948b3b
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-04-11 09:59:12 +0200

    added icon to remove task

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,29 +1,47 @@
 // Component to show and check one todo (a task)
+import { RxCross1 } from "react-icons/rx"
+
 import moment from "moment"
 import "./TodoItem.css"
 
 // passing the props (data = id, completed, onChange, todo, createdAt) from TodoList.jsx
-export const Todo = ({ id, completed, onChange, todo, createdAt, removeTodo }) => {
+export const Todo = ({
+  id,
+  completed,
+  onChange,
+  todo,
+  createdAt,
+  removeTodo,
+}) => {
   return (
     <div className="todo-item">
       <li
         key={id} // Use the unique id (= Date.now() thing) of each todo for the key prop.
         style={{ textDecoration: completed ? "line-through" : "none" }} // If the todo is completed, strike through the text; if not, display normally.
       >
-        <input
-          id={id}
-          className="checkbox"
-          type="checkbox"
-          checked={completed} // The checkbox is checked if the todo is marked as completed.
-          onChange={onChange} // When you click the checkbox, the toggleTodo function is called with the todo's ID to switch its "completed" status between done and not done.
-        />
-        <label htmlFor={id} className="todo-text">
-          {todo}
-        </label>
-        <div className="todo-time">
-          {moment(createdAt).format("YYYY-MM-DD HH:mm")}
+        <div className="list-item">
+          <div className="list-text">
+            <input
+              id={id}
+              className="checkbox"
+              type="checkbox"
+              checked={completed} // The checkbox is checked if the todo is marked as completed.
+              onChange={onChange} // When you click the checkbox, the toggleTodo function is called with the todo's ID to switch its "completed" status between done and not done.
+            />
+            <label htmlFor={id} className="todo-text">
+              {todo}
+            </label>
+            <div className="todo-time">
+              {moment(createdAt).format("YYYY-MM-DD HH:mm")}
+            </div>{" "}
+          </div>
+
+          <div className="remove-button">
+            <button onClick={() => removeTodo(id)}>
+              <RxCross1 className="remove-icon"/>
+            </button>
+          </div>
         </div>
-        <button onClick={() => removeTodo(id)}>Remove</button>
       </li>
     </div>
   )
