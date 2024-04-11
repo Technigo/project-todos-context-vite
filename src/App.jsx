a003ad990d4be29bcb0513f79752a1b78caa3460
izzibizz <contact@izabellind.com>
2024-04-11 13:32:35 +0200

    small styling updates

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import ToDoList from "./components/ToDoList";
 import { ToDoProvider } from "./context/ToDoContext";
 import { CompleteProvider } from "./context/CompleteContext";
 import CompleteList from "./components/CompleteList";
-import Greeting from './components/Greeting'
 import ToDoForm from './components/ToDoForm'
 import InspirationCard from "./components/InspirationCard";
 import "./styling/app.css";
