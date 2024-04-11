4a192b52fd48d336b2e0a8009fddd3a5b1debdc8
Alma Herrström <almaherrstrom@Almas-Air-4.lan>
2024-04-11 13:43:25 +0200

    change styling for completed tasks

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -17,7 +17,6 @@ export const Todo = ({
     <div className="todo-item">
       <li
         key={id} // Use the unique id (= Date.now() thing) of each todo for the key prop.
-        style={{ textDecoration: completed ? "line-through" : "none" }} // If the todo is completed, strike through the text; if not, display normally.
       >
         <div className="list-item">
           <div className="list-text">
@@ -28,10 +27,12 @@ export const Todo = ({
               checked={completed} // The checkbox is checked if the todo is marked as completed.
               onChange={onChange} // When you click the checkbox, the toggleTodo function is called with the todo's ID to switch its "completed" status between done and not done.
             />
-            <label htmlFor={id} className="todo-text">
+            <label
+              htmlFor={id}
+              className={`todo-text ${completed ? "completed" : ""}`}>
               {todo}
             </label>
-            <div className="todo-time">
+            <div className={`todo-time ${completed ? "completed" : ""}`}>
               {moment(createdAt).format("YYYY-MM-DD HH:mm")}
             </div>
           </div>
