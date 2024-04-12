2a552c31bcd9453927948922e76eb65b67d148e6
Paulajungaker <paula010514@hotmail.com>
2024-04-12 17:27:49 +0200

    Styling header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
 import "../styling/Header.css";
-import Logo from "../assets/5803966_checklist_clipboard_task_todo_icon.png";
 
 const Header = () => {
   return (
     <header className="headerContainer">
-      <img src={Logo} alt="logo" className="logo" />
-      <h1 className="header">Todo App</h1>
+      <h1 className="header">TaskBuddy</h1>
     </header>
   );
 };
