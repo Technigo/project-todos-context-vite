e01b7d2f508d62b49628fcb81e41d66e5beb916f
Johanna Billingskog Nyberg <johanna@billingskognyberg.se>
2024-04-12 12:15:47 +0200

    Replaced ul with a div and removed li.

--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -10,30 +10,28 @@ export const ToDoCard = ({ toDo, toggleComplete }) => {
   };
   return (
     <div className="to-do-list-item">
-      <li>
-        <label className="checkbox-container">
-          <input
-            type="checkbox"
-            checked={toDo.completed}
-            onChange={toggleComplete}
-          ></input>
-          <span
-            style={{
-              textDecoration: toDo.completed ? "line-through" : "none",
-              color: toDo.completed ? "#7A7777" : "inherit",
-            }}
-          >
-            <div className="todo-text">{toDo.description}</div>
-          </span>
-          <span className="checkmark"></span>
-        </label>
-        <img
-          src={removeIcon}
-          alt="Remove"
-          className="remove-button"
-          onClick={handleRemove}
-        />
-      </li>
+      <label className="checkbox-container">
+        <input
+          type="checkbox"
+          checked={toDo.completed}
+          onChange={toggleComplete}
+        ></input>
+        <span
+          style={{
+            textDecoration: toDo.completed ? "line-through" : "none",
+            color: toDo.completed ? "#7A7777" : "inherit",
+          }}
+        >
+          <div className="todo-text">{toDo.description}</div>
+        </span>
+        <span className="checkmark"></span>
+      </label>
+      <img
+        src={removeIcon}
+        alt="Remove"
+        className="remove-button"
+        onClick={handleRemove}
+      />
     </div>
   );
 };
