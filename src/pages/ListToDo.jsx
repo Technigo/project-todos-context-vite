2c6a735c11ef37e92b30ac91fac4c6c59a323949
Citronskal <yiaswift@gmail.com>
2024-04-13 09:50:21 +0200

    comment out back button

--- src/pages/ListToDo.jsx
+++ src/pages/ListToDo.jsx
@@ -72,12 +72,12 @@ export const ListToDo = () => {
 
   return (
     <section className="add-section">
-      <Link to="/" className="todo-link">
+      {/* <Link to="/" className="todo-link">
         <p>🔙</p>
-      </Link>
+      </Link> */}
 
       <div className="add-title">
-        <h2>All</h2>
+        <h1>Your to-do list</h1>
       </div>
       <div className="add-container">
         {todos.map((todo, index) => (
@@ -94,16 +94,18 @@ export const ListToDo = () => {
             >
               <div className="task-text">{todo.text}</div>
               <div className="task-date">{todo.date}</div>
-              <button
-                className="task-delete"
-                onClick={() => {
-                  deleteToDo(index);
-                }}
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                  <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                </svg>
-              </button>
+              <span>
+                <button
+                  className="task-delete"
+                  onClick={() => {
+                    deleteToDo(index);
+                  }}
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+                  </svg>
+                </button>
+              </span>
 
               {/* <div>{todo.category}</div> */}
             </label>
