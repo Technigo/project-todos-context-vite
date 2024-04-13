c1f8f9873eeb49a1c1ca85ba84266cc8f66af236
Your Name <you@example.com>
2024-04-14 01:37:37 +0200

    add some extra changes in the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,14 @@ import thumbtackInLine from "../assets/thumbtack-in-line.png";
 export const Header = () => {
   return (
     <div className="header-container">
-      <h1>To Doodle</h1>
-      <img src={thumbtackInLine} alt="thumbtack" className="thumbtack-in-line"></img>
+      <div>
+        <h1>To Doodle</h1>
+        <img
+          src={thumbtackInLine}
+          alt="thumbtack"
+          className="thumbtack-in-line"
+        ></img>
+      </div>
     </div>
   );
 };
