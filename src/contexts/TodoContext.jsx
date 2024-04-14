72259c0e7cd69132844ca92d575081baa889a05f
Pernilla Sterner <psterner@hotmail.com>
2024-04-14 18:49:42 +0200

    Save and handle todos in localstorage

--- src/contexts/TodoContext.jsx
+++ src/contexts/TodoContext.jsx
@@ -5,24 +5,35 @@
  * 2 step -> Add a new todo
  */
 
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
   // This variable will contain all the todos
-  const [todos, setTodos] = useState([
-    {
-      id: 4352345345,
-      description: "First todo....",
-      isCompleted: false,
-    },
-    {
-      id: 3983974832,
-      description: "Second todo....",
-      isCompleted: true,
-    },
-  ]);
+  const [todos, setTodos] = useState(() => {
+    // Load todos from localstorage if exists, otherwhise return default valuse
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos
+      ? JSON.parse(storedTodos)
+      : [
+          {
+            id: 4352345345,
+            description: "Learning about localstorage",
+            isCompleted: false,
+          },
+          {
+            id: 3983974832,
+            description: "Make todo app responsive",
+            isCompleted: true,
+          },
+        ];
+  });
+
+  useEffect(() => {
+    // Save todos in localStorage when there is a change
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   // Function that will add todos
   const addTodo = (todo) => {
