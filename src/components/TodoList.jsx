5130cf8829051ab94ece55554ea8949c39588df1
Katarina Sjölin <kat.sjolin@gmail.com>
2024-05-13 09:42:48 +0200

    added count of all todos and all uncompleted tasks

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -11,9 +11,15 @@ const TodoList = () => {
     toggleTodo(id)
   }
 
+  const totalTasksCount = todos.length
+
+  const uncompletedTasksCount = todos.filter(todo => !todo.completed).length
+
   return (
     <div>
       <h2>Todo List</h2>
+      <p>Total tasks: {totalTasksCount}</p>
+      <p>Uncompleted tasks: {uncompletedTasksCount}</p>
       <ul>
         {todos.map(todo => (
           <li key={todo.id}>
