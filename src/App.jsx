8c88e2a83aea7c4114f6a5c0c935a949ac705e51
Cornelia Dahlin <cornelia.dahlin@proton.me>
2024-05-23 11:33:54 +0200

    Removed reminder, added hide/show to workout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { TodoProvider } from "./Components/TodoContext/TodoContext.jsx";
 import { ListName } from "./Components/ListName/ListName.jsx";
 import { TodoList } from "./Components/TodoList/TodoList.jsx";
 import { WorkoutBlock } from "./Components/Workout/WorkoutBlock.jsx";
-import { ReminderBlock } from "./Components/Reminder/ReminderBlock.jsx";
 import "./index.css";
 
 export const App = () => {
+  const [isWorkoutBlockVisible, setIsWorkoutBlockVisible] = useState(true);
+
+  const toggleWorkoutBlockVisibility = () => {
+    setIsWorkoutBlockVisible(!isWorkoutBlockVisible);
+  };
+
   return (
     <TodoProvider>
       <div>
@@ -19,12 +25,22 @@ export const App = () => {
           <div className="first-block">
             <ListName />
           </div>
-          <div className="second-block">
-            <WorkoutBlock />
+          <div
+            className={`second-block ${
+              isWorkoutBlockVisible ? "expanded" : "collapsed"
+            }`}
+          >
+            <div className="second-block-content">
+              {isWorkoutBlockVisible && <WorkoutBlock />}
+            </div>
+            <button
+              className="toggle-button"
+              onClick={toggleWorkoutBlockVisibility}
+            >
+              {isWorkoutBlockVisible ? "Hide" : "Show"}
+            </button>
           </div>
-          <div className="third-block">
-            <ReminderBlock />
-          </div>
+          <div className="third-block"></div>
         </div>
       </div>
     </TodoProvider>
