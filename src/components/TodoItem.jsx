6004d65f37b05ff69f19c6438be5982303839bc1
KatSjolin <kat.sjolin@gmail.com>
2024-06-14 13:52:43 +0200

    fixed some minor flaws

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import TodoContext from './TodoContext';
+
+const TodoItem = ({ todo }) => {
+  const { toggleTodo, deleteTodo } = useContext(TodoContext);
+
+  const handleToggle = () => {
+    toggleTodo(todo.id);
+  };
+
+  const handleDelete = () => {
+    deleteTodo(todo.id);
+  };
+
+  return (
+    <li>
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={handleToggle}
+      />
+      <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+        {todo.text}
+      </span>
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
+};
+
+export default TodoItem;
